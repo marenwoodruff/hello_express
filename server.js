c7c7284b8d8cd9a0d015ca2b57613058f02f4249
MarenWoodruff <wren403@gmail.com>
2017-05-16 12:08:09 -0400

    updated hello express app with handlebars

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express'); // Loading the express module on our server
 var app = express(); // Creates a new instance of express on our server
+var hbs = require('hbs'); // loads handlebars
 
+app.set('view engine', 'hbs'); //tells Express what to use for rendering templates
+app.use(express.static(__dirname + '/public')); // VERY IMPORTANT!! Make sure to add a '/'
 
 app.get('/', function(req, res) { 
   // when a request comes in at localhost:3000, it will respond 
@@ -8,22 +11,40 @@ app.get('/', function(req, res) {
 });
 
 app.get('/greeting', function(req, res) {
-  res.send('HEY, WDI 10!');
-  console.log('req route: ' + req.route);
-  console.log('req query: ' + req.query);
+  // res.send('HEY, WDI 10!');
+  // console.log('req route: ' + req.route);
+  console.log('req query: ' + req.query.saying);
+
+  res.render('greeting', {
+    name: 'Maren',
+    data: req.query.saying
+  })
 });
 
 app.get('/rihanna', function(req, res) {
   res.send("Work work work work work");
 });
 
-app.get("/hello/:name", function(req, res){
+app.get('/favorite-foods', function(req,res) {
+  var favoriteFoods = ['ice cream', 'indian', 'coffee'];
+
+  res.render('favorite-foods', {
+    data: favoriteFoods
+  })
+});
+
+app.get("users/:id", function(req, res){
   console.log(req.params);
   console.log(req.route);
   console.log(req.query);
   // parameter is name, query parameter is first_name
   res.send(`Hello, my last name is ${req.params.name}. My first name is ${req.query.first_name}.`);
 });
+
+// app.get("users", function(req, res){
+
+// });
+
 
 var port = process.env.PORT || 3000; // tells the server where to listen for requests
 
